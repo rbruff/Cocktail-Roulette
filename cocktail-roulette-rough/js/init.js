96f2828057b991e57190e91c005706cb104a1b40
renee <reneebruff@gmail.com>
2018-03-25 15:16:19 -0400

    modals added

--- cocktail-roulette-rough/js/init.js
+++ cocktail-roulette-rough/js/init.js
@@ -4,5 +4,8 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
+      $(document).ready(function(){
+    $('.modal').modal();
+  });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
